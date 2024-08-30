64a2a3436742788a8f0b6eb5d0d2e1a3c344ee19
vroslmend <homesuper7@gmail.com>
2024-08-30 13:28:11 +0500

    Add button styling changes to Hero.tsx

--- aala-cart-storefront/components/Hero.tsx
+++ aala-cart-storefront/components/Hero.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { AspectRatio } from "./ui/aspect-ratio";
 import Image from "next/image";
+import { Button } from "./ui/button";
 
 const Hero = () => {
   return (
@@ -10,17 +11,17 @@ const Hero = () => {
           <div className="@[480px]:p-4">
             <div className="flex min-h-[480px] flex-col gap-6 bg-cover bg-center bg-no-repeat @[480px]:gap-8 @[480px]:rounded-xl items-start justify-end px-4 pb-10 @[480px]:px-10">
               <div className="flex flex-col gap-2 text-left">
-                <h1 className="text-white text-4xl font-black leading-tight tracking-[-0.033em] @[480px]:text-5xl @[480px]:font-black @[480px]:leading-tight @[480px]:tracking-[-0.033em]">
+                <h1 className="text-black text-4xl font-black leading-tight tracking-[-0.033em] @[480px]:text-5xl @[480px]:font-black @[480px]:leading-tight @[480px]:tracking-[-0.033em]">
                   Saira Collection
                 </h1>
-                <h2 className="text-white text-sm font-normal leading-normal @[480px]:text-base @[480px]:font-normal @[480px]:leading-normal">
+                <h2 className="text-black text-sm font-normal leading-normal @[480px]:text-base @[480px]:font-normal @[480px]:leading-normal">
                   A brand for the modern woman, offering a range of stylish and
                   comfortable clothing pieces made from high quality fabrics.
                 </h2>
               </div>
-              <button className="flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-xl h-10 px-4 @[480px]:h-12 @[480px]:px-5 bg-[#f49a25] text-[#1c150d] text-sm font-bold leading-normal tracking-[0.015em] @[480px]:text-base @[480px]:font-bold @[480px]:leading-normal @[480px]:tracking-[0.015em]">
+              <Button className="flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-xl h-10 px-4 @[480px]:h-12 @[480px]:px-5 bg-[#000000] text-[#fefefe] text-sm font-bold leading-normal tracking-[0.015em] @[480px]:text-base @[480px]:font-bold @[480px]:leading-normal @[480px]:tracking-[0.015em]">
                 <span className="truncate">Shop Now</span>
-              </button>
+              </Button>
             </div>
           </div>
         </div>
